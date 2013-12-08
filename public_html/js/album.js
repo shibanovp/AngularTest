02f4d96a0cff44b887ec5b9d81e25b0c632c291b
Paul <unkindest89@gmail.com>
2013-12-08 16:53:30 +0400

    AngularJS Album

---
+++ public_html/js/album.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function AlbumCtrl($scope){
+    //alert("d");
+    $scope.images =[
+        {image: 'img/project1@2x.png', thumbnail: 'img/project1.png',description:'Image 01 decription'},
+        {image: 'img/project2@2x.png', thumbnail: 'img/project2.png',description:'Image 02 decription'},
+        {image: 'img/project3@2x.png', thumbnail: 'img/project3.png',description:'Image 03 decription'},
+        {image: 'img/project4@2x.png', thumbnail: 'img/project4.png',description:'Image 04 decription'},
+        {image: 'img/project5@2x.png', thumbnail: 'img/project5.png',description:'Image 05 decription'}
+    ];
+    $scope.currentImage =_.first($scope.images);
+}
