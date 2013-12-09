9c7af139c32e4b6f932d4f29bccc46b7f922f92e
Paul <unkindest89@gmail.com>
2013-12-09 13:39:38 +0400

    simpulton tutorial album pt3

--- public_html/js/album.js
+++ public_html/js/album.js
@@ -3,28 +3,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-function AlbumCtrl($scope){
+function AlbumCtrl($scope,$http){
     //alert("d");
-    $scope.images =[
-        {category:'ones', image: 'img/project1@2x.png', thumbnail: 'img/project1.png',description:'Image 01 decription'},
-        {category:'twoes', image: 'img/project2@2x.png', thumbnail: 'img/project2.png',description:'Image 02 decription'},
-        {category:'threes', image: 'img/project3@2x.png', thumbnail: 'img/project3.png',description:'Image 03 decription'},
-        {category:'fours', image: 'img/project4@2x.png', thumbnail: 'img/project4.png',description:'Image 04 decription'},
-        {category:'fives', image: 'img/project5@2x.png', thumbnail: 'img/project5.png',description:'Image 05 decription'},
-        {category:'ones', image: 'img/project1@2x.png', thumbnail: 'img/project1.png',description:'Image 01 decription'},
-        {category:'twoes', image: 'img/project2@2x.png', thumbnail: 'img/project2.png',description:'Image 02 decription'},
-        {category:'threes', image: 'img/project3@2x.png', thumbnail: 'img/project3.png',description:'Image 03 decription'},
-        {category:'fours', image: 'img/project4@2x.png', thumbnail: 'img/project4.png',description:'Image 04 decription'},
-        {category:'fives', image: 'img/project5@2x.png', thumbnail: 'img/project5.png',description:'Image 05 decription'},
-        {category:'ones', image: 'img/project1@2x.png', thumbnail: 'img/project1.png',description:'Image 01 decription'},
-        {category:'twoes', image: 'img/project2@2x.png', thumbnail: 'img/project2.png',description:'Image 02 decription'},
-        {category:'threes', image: 'img/project3@2x.png', thumbnail: 'img/project3.png',description:'Image 03 decription'},
-        {category:'fours', image: 'img/project4@2x.png', thumbnail: 'img/project4.png',description:'Image 04 decription'},
-        {category:'fives', image: 'img/project5@2x.png', thumbnail: 'img/project5.png',description:'Image 05 decription'},
-    ];
-    $scope.currentImage =_.first($scope.images);
-    $scope.imageCategories = _.uniq(_.pluck($scope.images, 'category'));
+    $scope.url = 'images.json';
+        $scope.images = [];
+        $scope.currentImage ={};
+        $scope.imageCategories = [];
+    
+    
+    $scope.handleImagesLoaded  = function(data, status){
+        $scope.images = data;
+        $scope.currentImage =_.first($scope.images);
+        $scope.imageCategories = _.uniq(_.pluck($scope.images, 'category'));
+    }
+    $scope.fetch = function(){
+        $http.get($scope.url).success($scope.handleImagesLoaded);
+    }
     $scope.setCurrentImage = function(image){
         $scope.currentImage = image;
     }
+    $scope.fetch();
 }
