93094bdb49dbaaceaf7b3d22b250f7713da5f619
Paul <unkindest89@gmail.com>
2013-12-09 13:15:03 +0400

    simpulton tutorial album pt2

--- public_html/js/album.js
+++ public_html/js/album.js
@@ -6,13 +6,24 @@
 function AlbumCtrl($scope){
     //alert("d");
     $scope.images =[
-        {image: 'img/project1@2x.png', thumbnail: 'img/project1.png',description:'Image 01 decription'},
-        {image: 'img/project2@2x.png', thumbnail: 'img/project2.png',description:'Image 02 decription'},
-        {image: 'img/project3@2x.png', thumbnail: 'img/project3.png',description:'Image 03 decription'},
-        {image: 'img/project4@2x.png', thumbnail: 'img/project4.png',description:'Image 04 decription'},
-        {image: 'img/project5@2x.png', thumbnail: 'img/project5.png',description:'Image 05 decription'}
+        {category:'ones', image: 'img/project1@2x.png', thumbnail: 'img/project1.png',description:'Image 01 decription'},
+        {category:'twoes', image: 'img/project2@2x.png', thumbnail: 'img/project2.png',description:'Image 02 decription'},
+        {category:'threes', image: 'img/project3@2x.png', thumbnail: 'img/project3.png',description:'Image 03 decription'},
+        {category:'fours', image: 'img/project4@2x.png', thumbnail: 'img/project4.png',description:'Image 04 decription'},
+        {category:'fives', image: 'img/project5@2x.png', thumbnail: 'img/project5.png',description:'Image 05 decription'},
+        {category:'ones', image: 'img/project1@2x.png', thumbnail: 'img/project1.png',description:'Image 01 decription'},
+        {category:'twoes', image: 'img/project2@2x.png', thumbnail: 'img/project2.png',description:'Image 02 decription'},
+        {category:'threes', image: 'img/project3@2x.png', thumbnail: 'img/project3.png',description:'Image 03 decription'},
+        {category:'fours', image: 'img/project4@2x.png', thumbnail: 'img/project4.png',description:'Image 04 decription'},
+        {category:'fives', image: 'img/project5@2x.png', thumbnail: 'img/project5.png',description:'Image 05 decription'},
+        {category:'ones', image: 'img/project1@2x.png', thumbnail: 'img/project1.png',description:'Image 01 decription'},
+        {category:'twoes', image: 'img/project2@2x.png', thumbnail: 'img/project2.png',description:'Image 02 decription'},
+        {category:'threes', image: 'img/project3@2x.png', thumbnail: 'img/project3.png',description:'Image 03 decription'},
+        {category:'fours', image: 'img/project4@2x.png', thumbnail: 'img/project4.png',description:'Image 04 decription'},
+        {category:'fives', image: 'img/project5@2x.png', thumbnail: 'img/project5.png',description:'Image 05 decription'},
     ];
     $scope.currentImage =_.first($scope.images);
+    $scope.imageCategories = _.uniq(_.pluck($scope.images, 'category'));
     $scope.setCurrentImage = function(image){
         $scope.currentImage = image;
     }
